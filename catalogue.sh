b1b9761799b5363eaaf1079e582bd754d627e263
Shefali <shefalimurarka19@gmail.com>
2024-11-29 22:40:37 +0530

    Roboshop setup

--- catalogue.sh
+++ catalogue.sh
@@ -1,20 +1,10 @@
 component=catalogue
+app_path=/app
 
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
-cp catalogue.service /etc/systemd/system/catalogue.service
+NODEJS
+SYSTEMD_SETUP
+APP_PREREQ
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-useradd roboshop
-rm -rf /app
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
 npm install
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl restart catalogue
 dnf install mongodb-mongosh -y
 mongosh --host 172.31.89.237 </app/db/master-data.js
--- common.sh
+++ common.sh
@@ -5,8 +5,18 @@ code_dir=$(pwd)
 PRINT(){
   echo &>>$LOG_FILE
   echo &>>$LOG_FILE
-  echo "****************************$1***************************" &>>LOG_FILE
-  echo &>>LOG_FILE
+  echo "****************************$****************************" &>>LOG_FILE
+  echo $*
+}
+
+STAT(){
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSuccess\e[0m"
+  else
+    echo -e "\e[31mFailure\e[0m"
+    echo
+    echo "Refer the log file for more information : file path ${LOG_FILE}"
+  fi
 }
 
 SYSTEMD_SETUP(){
@@ -15,7 +25,47 @@ SYSTEMD_SETUP(){
       STAT $?
 
   PRINT Start Service
-     systemctl daemon-reload
-     systemctl enable ${component}
-     systemctl restart ${component}
+     systemctl daemon-reload &>>$LOG_FILE
+     systemctl enable ${component} &>>$LOG_FILE
+     systemctl restart ${component} &>>$LOG_FILE
+     STAT $?
+}
+NODEJS(){
+  PRINT Disable Default Version of nodejs
+  dnf module disable nodejs -y &>>LOG_FILE
+  STAT $?
+
+  PRINT Enable nodejs 20 module
+  dnf module enable nodejs:20 -y &>>LOG_FILE
+  STAT $?
+
+  PRINT Install nodejs
+  dnf install nodejs -y &>>LOG_FILE
+  STAT $?
+}
+
+APP_PREREQ(){
+  PRINT Adding Application User
+  id roboshop &>>LOG_FILE
+  if [ $? -ne 0 ] ; then
+    useradd roboshop &>>LOG_FILE
+  fi
+  STAT $?
+
+  PRINT Remove old content
+  rm -rf {$app_path} &>>LOG_FILE
+  STAT $?
+
+  PRINT Create APP Directory
+  mkdir {$app_path}  &>>LOG_FILE
+  STAT $?
+
+  PRINT Download Application Content
+  curl -o /tmp/{$component}.zip https://roboshop-artifacts.s3.amazonaws.com/{$component}-v3.zip &>>LOG_FILE
+  STAT $?
+
+  PRINT Extract Application Content
+  cd {app_path} &>>LOG_FILE
+  unzip /tmp/{component}.zip &>>LOG_FILE
+  STAT $?
 }
