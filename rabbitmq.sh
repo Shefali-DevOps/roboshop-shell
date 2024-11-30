7c1431b3a269821336bc48c338e7d80309081e50
Shefali <shefalimurarka19@gmail.com>
2024-11-30 15:18:03 +0530

    Roboshop setup

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,6 +1,20 @@
-cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
-dnf install rabbitmq-server -y
-systemctl enable rabbitmq-server
-systemctl restart rabbitmq-server
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+source common.sh
+component=rabbitmq
+
+PRINT Copy rabbitmq repo file
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>LOG_FILE
+STAT $?
+
+PRINT Install rabbitmq
+dnf install rabbitmq-server -y &>>LOG_FILE
+STAT $?
+
+PRINT Start Rabbitmq Service
+systemctl enable rabbitmq-server &>>LOG_FILE
+systemctl restart rabbitmq-server &>>LOG_FILE
+STAT $?
+
+PRINT add user and set permission
+rabbitmqctl add_user roboshop roboshop123 &>>LOG_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>LOG_FILE
+STAT $?
